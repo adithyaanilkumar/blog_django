42e70711820c8d4cf7c275accff696e1fa8107cc
root <root@LAPTOP-U13VQOH3.localdomain>
2020-02-01 22:01:37 +0530

    passing variables to templates

--- learn/blog/views.py
+++ learn/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts=[
+    {
+        'author':'Adithya',
+        'Title': 'blog post 1',
+        'content':'first post',
+        'date_posted':'August 27,2018',
+    },
+    {
+        'author':'amma',
+        'Title': 'blog post 2',
+        'content':'second post',
+        'date_posted':'August 28,2018',
+    }
+]
 
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
