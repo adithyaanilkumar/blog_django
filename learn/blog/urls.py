dc34ec79bff6521983ca936afa6b985821e8468e
root <root@LAPTOP-U13VQOH3.localdomain>
2020-02-01 17:01:28 +0530

    added paths home and blog

---
+++ learn/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('home/',views.home,name="blog_home")
+    path('home/',views.about,name="blog_about")
+
+]
---
+++ learn/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(response):
+    return HttpResponse('<h1>home</h1>')
+
+def about(response):
+    return HttpResponse('<h2>about</h2>')
