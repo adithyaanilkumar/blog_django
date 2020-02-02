668242b4def33bd2526e4d28e79d8449ac80263e
root <root@LAPTOP-U13VQOH3.localdomain>
2020-02-02 10:10:25 +0530

    before adding template inheritance

--- learn/blog/views.py
+++ learn/blog/views.py
@@ -20,7 +20,7 @@ def home(request):
     context={
         'posts':posts
     }
-    return render(request,'blog/home.html',context)
+    return render(request,'blog/home.html',context, {'title':"sup"})
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html', {'title':'hello'})
