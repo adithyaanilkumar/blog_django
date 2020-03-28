a7b8933f12c56832af4aed69724e0d27324e94f1
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-28 11:57:57 +0000

    code refactor

--- learn/blog/urls.py
+++ learn/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/',views.home,name="blog_home"),
-    path('about/',views.about,name="blog_about")
+    path('home/',views.home,name="blog-home"),
+    path('about/',views.about,name="blog-about")
 
 ]
